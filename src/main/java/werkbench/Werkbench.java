b5b02e86e674d12487832e298d9ad5ac63f918ed
jakimfett <jakimfett@picraft.com>
2015-01-13 19:14:11 -0800

    Add basic files

---
+++ src/main/java/werkbench/Werkbench.java
@@ -0,0 +1,10 @@
+package werkbench;
+
+import cpw.mods.fml.common.Mod;
+import werkbench.reference.Compendium;
+
+@Mod(modid = Compendium.Naming.id)
+public class Werkbench
+{
+
+}
---
+++ src/main/java/werkbench/reference/Config.java
@@ -0,0 +1,66 @@
+package werkbench.reference;
+
+import cpw.mods.fml.client.config.IConfigElement;
+import cpw.mods.fml.client.event.ConfigChangedEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import net.minecraft.util.StatCollector;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
+
+public class Config
+{
+    public static Configuration config;
+
+    // turns on extra logging printouts
+    public static boolean debugMode = true;
+
+    public static void init()
+    {
+
+        if (config == null)
+        {
+
+            config = new Configuration(new File(Compendium.Config.configPrefix + "Werkbench.cfg"));
+            loadConfig();
+        }
+    }
+
+    @SubscribeEvent
+    public void onConfigChangedEvent(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.modID.equalsIgnoreCase(Compendium.Naming.id))
+        {
+            loadConfig();
+        }
+    }
+
+    private static void loadConfig()
+    {
+        Property prop;
+        List<String> configList = new ArrayList<String>();
+
+        config.addCustomCategoryComment(Configuration.CATEGORY_GENERAL, StatCollector.translateToLocal("config.general.description"));
+
+        prop = config.get(Configuration.CATEGORY_GENERAL, "debugMode", Config.debugMode);
+        prop.comment = StatCollector.translateToLocal("config.debugMode");
+        prop.setLanguageKey("config.debugMode.tooltip");
+        debugMode = prop.getBoolean();
+        configList.add(prop.getName());
+
+        if (config.hasChanged())
+        {
+            config.save();
+        }
+    }
+
+    public static List<IConfigElement> getConfigElements()
+    {
+        List<IConfigElement> list = new ArrayList<IConfigElement>();
+        list.addAll(new ConfigElement(config.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements());
+        return list;
+    }
+}
