166c76a361c5b205da081c663e4bc60495897fa6
jakimfett <jakimfett@gmail.com>
2015-01-17 15:06:14 -0800

    Mimic crafting table functionality

--- src/main/java/werkbench/bench/BenchContainer.java
+++ src/main/java/werkbench/bench/BenchContainer.java
@@ -3,16 +3,42 @@ package werkbench.bench;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryCraftResult;
+import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.inventory.Slot;
+import net.minecraft.inventory.SlotCrafting;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.world.World;
 
 public class BenchContainer extends Container
 {
 	private final BenchTileEntity bench;
 
-	public BenchContainer(InventoryPlayer inventoryPlayer, BenchTileEntity bench)
+	public InventoryCrafting craftMatrix = new InventoryCrafting(this, 3, 3);
+	public IInventory craftResult = new InventoryCraftResult();
+	private World world;
+
+	public BenchContainer(InventoryPlayer inventoryPlayer, BenchTileEntity bench, World world)
 	{
+
+		this.world = world;
 		this.bench = bench;
-		this.bindPlayerInventory(inventoryPlayer);
+		bindPlayerInventory(inventoryPlayer);
+		bindCraftGrid(inventoryPlayer);
+		addSlotToContainer(new SlotCrafting(inventoryPlayer.player, this.craftMatrix, this.craftResult, 0, 131, 30));
+	}
+
+	private void bindCraftGrid(InventoryPlayer inventoryPlayer)
+	{
+		for (int i = 0; i < 3; ++i)
+		{
+			for (int j = 0; j < 3; ++j)
+			{
+				addSlotToContainer(new Slot(this.craftMatrix, j + i * 3, 62 + j * 18, 12 + i * 18));
+			}
+		}
 	}
 
 	private void bindPlayerInventory(InventoryPlayer inventoryPlayer)
@@ -31,9 +57,76 @@ public class BenchContainer extends Container
 		}
 	}
 
+	/**
+	 *
+	 * @param inventory
+	 */
+	public void onCraftMatrixChanged(IInventory inventory)
+	{
+		this.craftResult.setInventorySlotContents(0, CraftingManager.getInstance().findMatchingRecipe(this.craftMatrix, this.world));
+	}
+
 	@Override
 	public boolean canInteractWith(EntityPlayer entityPlayer)
 	{
 		return true;
 	}
+
+	/**
+	 * Called when a player shift-clicks on a slot.
+	 */
+	public ItemStack transferStackInSlot(EntityPlayer player, int slotID)
+	{
+		ItemStack itemstack = null;
+		Slot slot = (Slot) this.inventorySlots.get(slotID);
+
+		if (slot != null && slot.getHasStack())
+		{
+			ItemStack itemstack1 = slot.getStack();
+			itemstack = itemstack1.copy();
+
+			if (slotID == 0)
+			{
+				if (!this.mergeItemStack(itemstack1, 10, 46, true))
+				{
+					return null;
+				}
+
+				slot.onSlotChange(itemstack1, itemstack);
+			} else if (slotID >= 10 && slotID < 37)
+			{
+				if (!this.mergeItemStack(itemstack1, 37, 46, false))
+				{
+					return null;
+				}
+			} else if (slotID >= 37 && slotID < 46)
+			{
+				if (!this.mergeItemStack(itemstack1, 10, 37, false))
+				{
+					return null;
+				}
+			} else if (!this.mergeItemStack(itemstack1, 10, 46, false))
+			{
+				return null;
+			}
+
+			if (itemstack1.stackSize == 0)
+			{
+				slot.putStack((ItemStack) null);
+			} else
+			{
+				slot.onSlotChanged();
+			}
+
+			if (itemstack1.stackSize == itemstack.stackSize)
+			{
+				return null;
+			}
+
+			slot.onPickupFromSlot(player, itemstack1);
+		}
+
+		return itemstack;
+	}
+
 }
