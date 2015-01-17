8306598b218406ee827a1dbc57ad072c83dba0da
jakimfett <jakimfett@gmail.com>
2015-01-17 12:42:45 -0800

    Use block textures

--- src/main/java/werkbench/bench/BenchBlock.java
+++ src/main/java/werkbench/bench/BenchBlock.java
@@ -1,23 +1,66 @@
 package werkbench.bench;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
+import werkbench.reference.Compendium;
 
 public class BenchBlock extends BlockContainer
 {
-    public BenchBlock()
-    {
-        super(Material.wood);
-        setCreativeTab(CreativeTabs.tabDecorations);
-    }
+	@SideOnly(Side.CLIENT)
+	private IIcon topIcon;
+	@SideOnly(Side.CLIENT)
+	private IIcon frontIcon;
+	@SideOnly(Side.CLIENT)
+	private IIcon bottomIcon;
 
-    @Override
-    public TileEntity createNewTileEntity(World p_149915_1_, int p_149915_2_)
-    {
-        return new BenchTileEntity();
-    }
+	public BenchBlock()
+	{
+		super(Material.wood);
+		setCreativeTab(CreativeTabs.tabDecorations);
+		setStepSound(Block.soundTypeWood);
+		textureName = Compendium.Naming.id + ":werkBenchIcon";
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(World p_149915_1_, int p_149915_2_)
+	{
+		return new BenchTileEntity();
+	}
+
+	/**
+	 * Gets the block's texture. Args: side, meta
+	 */
+	@SideOnly(Side.CLIENT)
+	public IIcon getIcon(int side, int meta)
+	{
+		switch (side)
+		{
+			case 0:
+				return bottomIcon;
+			case 1:
+				return topIcon;
+			case 3:
+				return frontIcon;
+			default:
+				return blockIcon;
+		}
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister iconRegister)
+	{
+		this.topIcon = iconRegister.registerIcon(this.getTextureName());
+		this.blockIcon = iconRegister.registerIcon(this.getTextureName() + "Side");
+		this.frontIcon = iconRegister.registerIcon(this.getTextureName() + "Front");
+		this.bottomIcon = iconRegister.registerIcon(this.getTextureName() + "Bottom");
+	}
 
 }
