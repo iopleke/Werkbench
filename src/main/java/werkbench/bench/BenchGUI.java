4cfbec9697370c56fd612d2d8cee833bbaaab859
jakimfett <jakimfett@picraft.com>
2015-01-18 14:59:21 -0800

    Render inventory background only if there's a chest on the correct side

--- src/main/java/werkbench/bench/BenchGUI.java
+++ src/main/java/werkbench/bench/BenchGUI.java
@@ -7,10 +7,12 @@ import werkbench.reference.Compendium;
 
 public class BenchGUI extends GuiContainer
 {
+    BenchTileEntity bench;
 
     public BenchGUI(InventoryPlayer inventoryPlayer, BenchTileEntity bench, World world)
     {
         super(new BenchContainer(inventoryPlayer, bench, world));
+        this.bench = bench;
 
     }
 
@@ -19,8 +21,49 @@ public class BenchGUI extends GuiContainer
     {
         this.mc.renderEngine.bindTexture(Compendium.Texture.GUI.background);
         int x = (width - xSize) / 2;
-        int y = (height - ySize) / 2;
+        int y = (height - ySize) / 2 + 30;
         drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
+
+        if (bench.getHasChestLeft())
+        {
+            renderSingleChestLeft();
+        }
+        if (bench.getHasChestRight())
+        {
+            renderSingleChestRight();
+        }
+    }
+
+    private void renderDoubleChestLeft()
+    {
+        this.mc.renderEngine.bindTexture(Compendium.Texture.GUI.doubleChest);
+        int x = (width - xSize) / 2 - 122;
+        int y = (height - ySize) / 2 + 20;
+        drawTexturedModalRect(x, y, 0, 0, 122, 176);
+    }
+
+    private void renderSingleChestLeft()
+    {
+        this.mc.renderEngine.bindTexture(Compendium.Texture.GUI.singleChest);
+        int x = (width - xSize) / 2 - 68;
+        int y = (height - ySize) / 2 + 20;
+        drawTexturedModalRect(x, y, 0, 0, 68, 176);
+    }
+
+    private void renderDoubleChestRight()
+    {
+        this.mc.renderEngine.bindTexture(Compendium.Texture.GUI.doubleChest);
+        int x = (width - xSize) / 2 + 176;
+        int y = (height - ySize) / 2 + 20;
+        drawTexturedModalRect(x, y, 0, 0, 122, 176);
+    }
+
+    private void renderSingleChestRight()
+    {
+        this.mc.renderEngine.bindTexture(Compendium.Texture.GUI.singleChest);
+        int x = (width - xSize) / 2 + 176;
+        int y = (height - ySize) / 2 + 20;
+        drawTexturedModalRect(x, y, 0, 0, 68, 176);
     }
 
 }
