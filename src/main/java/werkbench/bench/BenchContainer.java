593f1374044e0183b03478994dd0a32f5b908ebe
jakimfett <jakimfett@picraft.com>
2015-04-19 20:50:54 -0700

    Refactor to use parameter based sides

--- src/main/java/werkbench/bench/BenchContainer.java
+++ src/main/java/werkbench/bench/BenchContainer.java
@@ -48,26 +48,8 @@ public final class BenchContainer extends Container
         bindPlayerInventory(inventoryPlayer);
         bindCraftGrid(inventoryPlayer);
 
-        AdjacentBlockType leftBlock = SpatialHelper.getBlockForRelativeSide(bench, RelativeBenchSide.LEFT);
-        AdjacentBlockType rightBlock = SpatialHelper.getBlockForRelativeSide(bench, RelativeBenchSide.RIGHT);
-
-        if (leftBlock == AdjacentBlockType.CHEST_SINGLE || leftBlock == AdjacentBlockType.CHEST_DOUBLE)
-        {
-            bindLeftChest();
-        }
-
-        if (leftBlock == AdjacentBlockType.FURNACE_INACTIVE || leftBlock == AdjacentBlockType.FURNACE_ACTIVE)
-        {
-            bindLeftFurnace(inventoryPlayer);
-        }
-        if (rightBlock == AdjacentBlockType.CHEST_SINGLE || rightBlock == AdjacentBlockType.CHEST_DOUBLE)
-        {
-            bindRightChest();
-        }
-        if (rightBlock == AdjacentBlockType.FURNACE_INACTIVE || rightBlock == AdjacentBlockType.FURNACE_ACTIVE)
-        {
-            bindRightFurnace(inventoryPlayer);
-        }
+        bindTileEntitySlotsForSide(inventoryPlayer, RelativeBenchSide.LEFT);
+        bindTileEntitySlotsForSide(inventoryPlayer, RelativeBenchSide.RIGHT);
     }
 
     /**
@@ -103,82 +85,6 @@ public final class BenchContainer extends Container
         addSlotToContainer(new SlotCrafting(inventoryPlayer.player, this.craftMatrix, this.craftResult, 0, 253, 70));
     }
 
-    private void bindLeftChest()
-    {
-        if (bench.isChestDouble(SpatialHelper.getDirectionFromRelativeSide(bench, RelativeBenchSide.LEFT)))
-        {
-            bindLeftChestDouble(bench);
-        } else
-        {
-            bindLeftChestSingle(bench);
-        }
-    }
-
-    private void bindLeftChestDouble(BenchTileEntity bench)
-    {
-        bindLeftChestSingle(bench);
-
-        TileEntity tileEntity = SpatialHelper.getTileEntityForRelativeSide(bench, RelativeBenchSide.LEFT, 2);
-        if (tileEntity instanceof TileEntityChest)
-        {
-            int slot, x, y;
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    slot = j + i * 9;
-                    x = 8 + i * 18;
-                    y = 38 + j * 18;
-                    addSlotToContainer(new Slot(((TileEntityChest) tileEntity), slot, x, y));
-                }
-            }
-        }
-    }
-
-    /**
-     * Add the left chest slots
-     *
-     * @param BenchTileEntity the bench tile entity
-     */
-    private void bindLeftChestSingle(BenchTileEntity bench)
-    {
-        TileEntity tileEntity = SpatialHelper.getTileEntityForRelativeSide(bench, RelativeBenchSide.LEFT);
-        if (tileEntity instanceof TileEntityChest)
-        {
-            int slot, x, y, count;
-            count = 0;
-            int[] slotArray = new int[27];
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    slot = j + i * 9;
-                    x = 62 + i * 18;
-                    y = 38 + j * 18;
-                    slotArray[count] = slot;
-                    addSlotToContainer(new Slot(((TileEntityChest) tileEntity), slot, x, y));
-                }
-            }
-
-            directionalSlots.put(SpatialHelper.getDirectionFromRelativeSide(bench, RelativeBenchSide.LEFT), slotArray);
-        }
-    }
-
-    private void bindLeftFurnace(InventoryPlayer inventoryPlayer)
-    {
-
-        TileEntity tileEntity = SpatialHelper.getTileEntityForRelativeSide(bench, RelativeBenchSide.LEFT);
-        if (tileEntity instanceof TileEntityFurnace)
-        {
-            // @TODO - make slot positioning less of a black box
-            int x = 55;
-            int y = 51;
-            addSlotToContainer(new Slot(((TileEntityFurnace) tileEntity), 0, x, y));
-            addSlotToContainer(new Slot(((TileEntityFurnace) tileEntity), 1, x + 21, y + 44));
-            addSlotToContainer(new SlotFurnace(inventoryPlayer.player, ((TileEntityFurnace) tileEntity), 2, x + 42, y));
-        }
-    }
-
     /**
      * Add the player's inventory slots to the GUI
      *
@@ -205,113 +111,54 @@ public final class BenchContainer extends Container
         }
     }
 
-    private void bindRightChest()
+    private void bindSlotsForChest(TileEntityChest chest, RelativeBenchSide side, AdjacentBlockType chestType)
     {
-        if (bench.isChestDouble(SpatialHelper.getDirectionFromRelativeSide(bench, RelativeBenchSide.RIGHT)))
-        {
-            bindRightChestDouble(bench);
-        } else
-        {
-            bindRightChestSingle(bench);
-        }
-    }
+        int slot, xOffset, yOffset, count = 0;
+        int[] guiOffsets;
 
-    private void bindRightChestDouble(BenchTileEntity bench)
-    {
-        bindRightChestSingle(bench);
+        int[] slotArray = new int[27];
 
-        TileEntity tileEntity = SpatialHelper.getTileEntityForRelativeSide(bench, RelativeBenchSide.RIGHT, 2);
-        if (tileEntity instanceof TileEntityChest)
+        for (int i = 0; i < 3; i++)
         {
-            int slot, x, y;
-            for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 9; j++)
             {
-                for (int j = 0; j < 9; j++)
-                {
-                    slot = j + i * 9;
-                    x = 360 + i * 18;
-                    y = 38 + j * 18;
-                    addSlotToContainer(new Slot(((TileEntityChest) tileEntity), slot, x, y));
-                }
+                slot = j + i * 9;
+                xOffset = i * 18;
+                yOffset = j * 18;
+                slotArray[count++] = slot;
+                guiOffsets = AdjacentBlockType.getGUICoordinate(side, chestType);
+                addSlotToContainer(new Slot(chest, slot, guiOffsets[0] + xOffset, guiOffsets[1] + yOffset));
             }
         }
-
+        directionalSlots.put(SpatialHelper.getDirectionFromRelativeSide(bench, RelativeBenchSide.LEFT), slotArray);
     }
 
-    /**
-     * Add the right chest slots
-     *
-     * @param BenchTileEntity the bench tile entity
-     */
-    private void bindRightChestSingle(BenchTileEntity bench)
+    private void bindSlotsForFurnace(TileEntityFurnace furnace, InventoryPlayer inventoryPlayer, RelativeBenchSide side)
     {
-        // @TODO - fix naming for getting the tile entities
+        int slot = 0;
+        int[] guiOffsets = AdjacentBlockType.getGUICoordinate(side, AdjacentBlockType.FURNACE_INACTIVE);
+        addSlotToContainer(new Slot(furnace, slot++, guiOffsets[0], guiOffsets[1]));
+        addSlotToContainer(new Slot(furnace, slot++, guiOffsets[0] + 21, guiOffsets[1] + 44));
+        addSlotToContainer(new SlotFurnace(inventoryPlayer.player, furnace, slot++, guiOffsets[0] + 42, guiOffsets[1]));
+    }
 
-        TileEntity tileEntity = SpatialHelper.getTileEntityForRelativeSide(bench, RelativeBenchSide.RIGHT);
+    private void bindTileEntitySlotsForSide(InventoryPlayer inventoryPlayer, RelativeBenchSide side)
+    {
+        TileEntity tileEntity = SpatialHelper.getTileEntityForRelativeSide(bench, side);
         if (tileEntity instanceof TileEntityChest)
         {
-            int slot, x, y;
-            for (int i = 0; i < 3; i++)
+            bindSlotsForChest(((TileEntityChest) tileEntity), side, AdjacentBlockType.CHEST_SINGLE);
+            TileEntity offsetTileEntity = SpatialHelper.getTileEntityForRelativeSide(bench, side, 2);
+            if (offsetTileEntity instanceof TileEntityChest)
             {
-                for (int j = 0; j < 9; j++)
-                {
-                    slot = j + i * 9;
-                    x = 306 + i * 18;
-                    y = 38 + j * 18;
-                    addSlotToContainer(new Slot(((TileEntityChest) tileEntity), slot, x, y));
-                }
+                bindSlotsForChest(((TileEntityChest) offsetTileEntity), side, AdjacentBlockType.CHEST_DOUBLE);
             }
-
-        }
-    }
-
-    private void bindRightFurnace(InventoryPlayer inventoryPlayer)
-    {
-        TileEntity tileEntity = SpatialHelper.getTileEntityForRelativeSide(bench, RelativeBenchSide.RIGHT);
-        if (tileEntity instanceof TileEntityFurnace)
+        } else if (tileEntity instanceof TileEntityFurnace)
         {
-            // @TODO - make slot positioning less of a black box
-            int x = 307;
-            int y = 51;
-            addSlotToContainer(new Slot(((TileEntityFurnace) tileEntity), 0, x, y));
-            addSlotToContainer(new Slot(((TileEntityFurnace) tileEntity), 1, x + 21, y + 44));
-            addSlotToContainer(new SlotFurnace(inventoryPlayer.player, ((TileEntityFurnace) tileEntity), 2, x + 42, y));
+            bindSlotsForFurnace(((TileEntityFurnace) tileEntity), inventoryPlayer, side);
         }
     }
 
-//	private void bindSlotsForChest(TileEntityChest chest, RelativeBenchSide side)
-//	{
-//		int slot, xOffset, yOffset;
-//		int[] guiOffsets;
-//		for (int i = 0; i < 3; i++)
-//		{
-//			for (int j = 0; j < 9; j++)
-//			{
-//				slot = j + i * 9;
-//				xOffset = i * 18;
-//				yOffset = j * 18;
-//				guiOffsets = AdjacentBlockType.getGUICoordinate(side, AdjacentBlockType.CHEST_SINGLE);
-//				addSlotToContainer(new Slot(chest, slot, guiOffsets[0] + xOffset, guiOffsets[1] + yOffset));
-//			}
-//		}
-//	}
-//
-//	private void bindTileEntitySlotsForSide(RelativeBenchSide side)
-//	{
-//		TileEntity tileEntity = SpatialHelper.getTileEntityForRelativeSide(bench, side);
-//		if (tileEntity instanceof TileEntityChest)
-//		{
-//			bindSlotsForChest(((TileEntityChest) tileEntity), side);
-//			TileEntity offsetTileEntity = SpatialHelper.getTileEntityForRelativeSide(bench, side, 2);
-//			if (offsetTileEntity instanceof TileEntityChest)
-//			{
-//				bindSlotsForChest(((TileEntityChest) offsetTileEntity), side);
-//			}
-//		} else if (tileEntity instanceof TileEntityFurnace)
-//		{
-//
-//		}
-//	}
     protected void resetSlotsForDirection(ForgeDirection direction)
     {
         int[] slotArray = this.directionalSlots.get(direction);
@@ -422,5 +269,4 @@ public final class BenchContainer extends Container
 
         return null;
     }
-
 }
