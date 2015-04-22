9732b321531eac0059bdc90dc7e72284f32197df
jakimfett <jakimfett@picraft.com>
2015-04-21 23:57:44 -0700

    Register furnace update messages

--- src/main/java/werkbench/network/MessageHandler.java
+++ src/main/java/werkbench/network/MessageHandler.java
@@ -5,7 +5,8 @@ import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.relauncher.Side;
-import werkbench.network.message.BenchUpdateMessage;
+import werkbench.network.message.FurnaceUpdateRequestMessage;
+import werkbench.network.message.FurnaceUpdateResponseMessage;
 import werkbench.reference.Compendium;
 
 /**
@@ -20,7 +21,8 @@ public class MessageHandler implements IMessageHandler
     public static void init()
     {
         int packetID = 0;
-        INSTANCE.registerMessage(BenchUpdateMessage.class, BenchUpdateMessage.class, packetID++, Side.SERVER);
+        INSTANCE.registerMessage(FurnaceUpdateRequestMessage.class, FurnaceUpdateRequestMessage.class, packetID++, Side.SERVER);
+        INSTANCE.registerMessage(FurnaceUpdateResponseMessage.class, FurnaceUpdateResponseMessage.class, packetID++, Side.CLIENT);
     }
 
     @Override
