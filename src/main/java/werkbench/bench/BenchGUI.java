c17f844f04d81827b74c399cb8a5db529c9ed18e
jakimfett <jakimfett@picraft.com>
2015-05-14 00:00:55 -0700

    Use new tabs class

--- src/main/java/werkbench/bench/BenchGUI.java
+++ src/main/java/werkbench/bench/BenchGUI.java
@@ -1,40 +1,31 @@
 package werkbench.bench;
 
+import jakimbox.prefab.gui.Tabs;
+import jakimbox.prefab.gui.Tabs.TabSide;
+import jakimbox.prefab.gui.tabTypes.ChestTab;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.world.World;
-import werkbench.gui.Tab;
-import werkbench.helper.LogHelper;
-import werkbench.helper.SpatialHelper;
-import werkbench.network.MessageHandler;
-import werkbench.network.message.FurnaceUpdateRequestMessage;
 import werkbench.reference.Compendium;
-import werkbench.reference.Compendium.AdjacentBlockType;
-import werkbench.reference.Compendium.RelativeBenchSide;
-import werkbench.reference.Config;
 
 public class BenchGUI extends GuiContainer
 {
     private BenchTileEntity bench;
-    private boolean doFurnaceUpdate;
-    private Tab[] tabs;
-    private int tickCount;
+    private Tabs tabs;
     private int xOffset;
     private int yOffset;
 
     public BenchGUI(InventoryPlayer inventoryPlayer, BenchTileEntity bench, World world)
     {
         super(new BenchContainer(inventoryPlayer, bench));
+        tabs = new Tabs(2);
+        tabs.addTab(new ChestTab(TabSide.LEFT), 0);
         this.bench = bench;
 
         this.xSize = 420;
         this.ySize = 206;
 
         updateOffsetCoordinates();
-        resetTickCount();
-        resetTabs();
     }
 
     private void bindGUITexture()
@@ -42,230 +33,51 @@ public class BenchGUI extends GuiContainer
         this.mc.renderEngine.bindTexture(Compendium.Resource.GUI.background);
     }
 
-    private void doTabClicks(int clickX, int clickY)
-    {
-        LogHelper.debug("Clicked at x:" + clickX + " y:" + clickY);
-
-        for (Tab tab : tabs)
-        {
-            if (tab != null)
-            {
-                if (tab.intersectsWithTab(clickX, clickY))
-                {
-                    LogHelper.debug("Tab on " + tab.getTabSide().toString() + " was clicked!");
-                    tab.initializeTabAnimation();
-                }
-            }
-        }
-    }
-
-    private void doTabToolTips(int mouseX, int mouseY)
-    {
-        for (Tab tab : tabs)
-        {
-            if (tab != null)
-            {
-                if (tab.intersectsWithTab(mouseX, mouseY))
-                {
-                    //LogHelper.debug("Tab on " + tab.getTabSide().toString() + " is hovered!");
-                    drawHoveringText(tab.tooltipForTab(mouseX, mouseY), mouseX, mouseY, mc.fontRenderer);
-                }
-            }
-        }
-    }
-
-    private void drawBackgroundForSide(RelativeBenchSide side)
-    {
-        AdjacentBlockType sideBlock = SpatialHelper.getBlockForRelativeSide(bench, side);
-        switch (sideBlock)
-        {
-            case CHEST_SINGLE:
-                renderSingleChestForSide(side);
-                break;
-            case CHEST_DOUBLE:
-                renderDoubleChestForSide(side);
-                break;
-            case FURNACE_ACTIVE:
-            case FURNACE_INACTIVE:
-                renderFurnaceForSide(side);
-                break;
-            default:
-                // do nothing
-                break;
-        }
-    }
-
     private void drawBenchBackground()
     {
         drawTexturedModalRect(xOffset + 98, yOffset - 50, 0, 0, 222, 256);
-    }
-
-    private void incrementTickCount()
-    {
-        tickCount++;
-        if (tickCount >= Config.furnaceGUIUpdatePacketFrequency)
-        {
-            resetTickCount();
-        }
-    }
-
-    private void renderDoubleChestForSide(RelativeBenchSide side)
-    {
-        this.mc.renderEngine.bindTexture(Compendium.Resource.GUI.doubleChest);
-        int[] guiOffsets = AdjacentBlockType.getGUIBackgroundCoordinates(side, AdjacentBlockType.CHEST_DOUBLE);
-        drawTexturedModalRect(xOffset + guiOffsets[0], yOffset + guiOffsets[1], 0, 0, 122, 176);
-
-    }
-
-    private void renderFurnaceForSide(RelativeBenchSide side)
-    {
-        if (doFurnaceUpdate)
-        {
-            sendFurnaceGUIUpdateRequest(side);
-        }
-        this.mc.renderEngine.bindTexture(Compendium.Resource.GUI.furnace);
-        int[] guiOffsets = AdjacentBlockType.getGUIBackgroundCoordinates(side, AdjacentBlockType.FURNACE_ACTIVE);
-
-        renderFurnaceProgressBars(side, xOffset + guiOffsets[0], yOffset + guiOffsets[1]);
-        renderFurnaceForeground(xOffset + guiOffsets[0], yOffset + guiOffsets[1]);
-
-    }
-
-    private void renderFurnaceForeground(int x, int y)
-    {
-        drawTexturedModalRect(x, y, 0, 0, 76, 76);
-    }
-
-    private void renderFurnaceProgressBars(RelativeBenchSide side, int x, int y)
-    {
-        int[] furnaceSideValues = bench.getFurnaceValuesForSide(side);
-        int cookProgress = 0;
-        int burnLevel = 0;
-        if (furnaceSideValues != null)
-        {
-            if (furnaceSideValues[0] > 0 && furnaceSideValues[2] > 0)
-            {
-                burnLevel = (furnaceSideValues[0] * 29 / furnaceSideValues[2]);
-            }
-            if (furnaceSideValues[1] > 0)
-            {
-                cookProgress = (furnaceSideValues[1] * 22 / 200);
-            }
-        }
-        drawTexturedModalRect(x, y, 0, 105, 76, 76);
-        if (SpatialHelper.getBlockForRelativeSide(bench, side) == AdjacentBlockType.FURNACE_ACTIVE)
-        {
-            if (burnLevel != 0)
-            {
-                drawTexturedModalRect(x, y + 76 - burnLevel, 0, 76 + 14 - burnLevel / 2, 76, burnLevel);
-            }
-            if (cookProgress != 0)
-            {
-                drawTexturedModalRect(x + 26, y + 12, 76, 0, cookProgress, 15);
-            }
-        }
-
-        drawTexturedModalRect(x, y, 0, 0, 76, 76);
-    }
-
-    private void renderSideBackgrounds()
-    {
-        for (Tab tab : tabs)
-        {
-            if (tab != null)
-            {
-                tab.renderTab();
-            }
-        }
-    }
-
-    private void renderSingleChestForSide(RelativeBenchSide side)
-    {
-//        int[] guiOffsets = AdjacentBlockType.getGUIBackgroundCoordinates(side, AdjacentBlockType.CHEST_SINGLE);
-//        if (side == RelativeBenchSide.LEFT)
-//        {
-//            leftTabs[0] = new Tab(this, AdjacentBlockType.CHEST_SINGLE, side);
-//            leftTabs[0].setTabGUIOffsetDefaults(xOffset + guiOffsets[0], yOffset + guiOffsets[1]);
-//        } else
-//        {
-//            rightTabs[0] = new Tab(this, AdjacentBlockType.CHEST_SINGLE, side);
-//            rightTabs[0].setTabGUIOffsetDefaults(xOffset + guiOffsets[0], yOffset + guiOffsets[1]);
-//        }
-    }
-
-    private void resetTabs()
-    {
-        tabs = new Tab[10];
-    }
-
-    private void resetTickCount()
-    {
-        tickCount = 0;
-        doFurnaceUpdate = true;
-    }
-
-    private void sendFurnaceGUIUpdateRequest(RelativeBenchSide side)
-    {
-        TileEntity tileEntity = SpatialHelper.getTileEntityForRelativeSide(bench, side);
-        if (tileEntity instanceof TileEntityFurnace)
-        {
-            MessageHandler.INSTANCE.sendToServer(new FurnaceUpdateRequestMessage(((TileEntityFurnace) tileEntity), bench, side));
-        }
     }
 
     private void updateOffsetCoordinates()
     {
         xOffset = (width - xSize) / 2;
         yOffset = (height - ySize) / 2;
+        tabs.setDefaultGUICoordinates(xOffset, yOffset);
     }
 
-    private void updateTabsForSide(RelativeBenchSide side)
+    private void updateTabs()
     {
-        if (SpatialHelper.getBlockForRelativeSide(bench, side) != null)
-        {
-            if (tabs[RelativeBenchSide.getArraySlot(side)] == null)
-            {
-                resetTabForSide(side);
-            } else if (tabs[RelativeBenchSide.getArraySlot(side)].getBlockType() != SpatialHelper.getBlockForRelativeSide(bench, side))
-            {
-                resetTabForSide(side);
-            }
-
-        }
-    }
-
-    private void resetTabForSide(RelativeBenchSide side)
-    {
-        tabs[RelativeBenchSide.getArraySlot(side)] = new Tab(this, SpatialHelper.getBlockForRelativeSide(bench, side), side);
+//        if (block on side != null)
+//        {
+//            if (tab for block == null)
+//            {
+//                tabs.addTab(new tab for block on side, id);
+//            } else if (tab in memory type != block on side tab type)
+//            {
+//                tabs.remove(id);
+//                tabs.addTab(null, width);
+//            }
+//
+//        }
     }
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float opacity, int mouseX, int mouseY)
     {
-        incrementTickCount();
         updateOffsetCoordinates();
         bindGUITexture();
         drawBenchBackground();
+        updateTabs();
+        tabs.renderTabs(this);
 
-        updateTabsForSide(RelativeBenchSide.LEFT);
-        updateTabsForSide(RelativeBenchSide.RIGHT);
-        updateTabsForSide(RelativeBenchSide.BACK);
-        drawBackgroundForSide(RelativeBenchSide.LEFT);
-        drawBackgroundForSide(RelativeBenchSide.RIGHT);
-        renderSideBackgrounds();
+        drawHoveringText(tabs.getTabToolTip(mouseX, mouseY), mouseX, mouseY, mc.fontRenderer);
 
-        doTabToolTips(mouseX, mouseY);
-
-        if (doFurnaceUpdate)
-        {
-            doFurnaceUpdate = false;
-        }
     }
 
     @Override
     protected void mouseClicked(int clickX, int clickY, int button)
     {
         super.mouseClicked(clickX, clickY, button);
-        doTabClicks(clickX, clickY);
+        tabs.doTabClicks(clickX, clickY);
     }
 }
