348fa0491655855e86e8c6f9f9f9c24eb8697f1d
jakimfett <jakimfett@picraft.com>
2015-05-16 12:02:17 -0700

    Convert Block to use JakimBox

--- src/main/java/werkbench/bench/BenchBlock.java
+++ src/main/java/werkbench/bench/BenchBlock.java
@@ -1,35 +1,26 @@
 package werkbench.bench;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+import jakimbox.prefab.block.BasicBlockContainer;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.IIcon;
-import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import werkbench.Werkbench;
 import werkbench.proxy.CommonProxy;
 import werkbench.reference.Compendium;
 
-public class BenchBlock extends BlockContainer
+public class BenchBlock extends BasicBlockContainer
 {
-    // @TODO - override breakBlock to make the TileEntity drop the inventory
+
     public BenchBlock()
     {
-        super(Material.wood);
-        setCreativeTab(CreativeTabs.tabDecorations);
+        super(Compendium.Naming.id, Compendium.Naming.name, Material.wood, CreativeTabs.tabDecorations);
         setStepSound(Block.soundTypeWood);
-        setBlockName(Compendium.Naming.id);
-        textureName = Compendium.Naming.id + ":werkBenchIcon";
     }
 
     @Override
@@ -71,30 +62,10 @@ public class BenchBlock extends BlockContainer
         return new BenchTileEntity();
     }
 
-    /**
-     * Gets the texture, given a side
-     *
-     * @param side the block side
-     * @param meta the block metadata
-     * @return IIcon
-     */
-    @Override
-    @SideOnly(Side.CLIENT)
-    public IIcon getIcon(int side, int meta)
-    {
-        return blockIcon;
-    }
-
     @Override
     public int getRenderType()
     {
         return CommonProxy.RENDER_ID;
-    }
-
-    @Override
-    public boolean isOpaqueCube()
-    {
-        return false;
     }
 
     /**
@@ -131,25 +102,6 @@ public class BenchBlock extends BlockContainer
     }
 
     /**
-     * Set the direction the block is facing
-     *
-     * @param world        the world object
-     * @param x            the changed block's x coordinate
-     * @param y            the changed block's y coordinate
-     * @param z            the changed block's z coordinate
-     * @param livingEntity entity that placed the block
-     * @param itemStack    the itemstack used to place the block
-     */
-    @Override
-    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase livingEntity, ItemStack itemStack)
-    {
-        super.onBlockPlacedBy(world, x, y, z, livingEntity, itemStack);
-
-        int facing = MathHelper.floor_double(livingEntity.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
-        world.setBlockMetadataWithNotify(x, y, z, facing, 2);
-    }
-
-    /**
      * Check if changedBlock is a chest, update GUI to reflect this
      *
      * @param world        the world object
@@ -166,23 +118,5 @@ public class BenchBlock extends BlockContainer
         {
             ((BenchTileEntity) potentialBench).updateSideChecks();
         }
-    }
-
-    /**
-     * Register the block icons for top, sides, front, and bottom
-     *
-     * @param iconRegister object for icon registration
-     */
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void registerBlockIcons(IIconRegister iconRegister)
-    {
-        this.blockIcon = iconRegister.registerIcon(this.getTextureName());
-    }
-
-    @Override
-    public boolean renderAsNormalBlock()
-    {
-        return false;
     }
 }
