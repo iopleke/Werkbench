26c9380913d6516900fa2d28f90a6b7228559f0b
jakimfett <jakimfett@picraft.com>
2015-05-19 22:23:06 -0700

    A bit of refactoring, a bit of trimming

--- src/main/java/werkbench/reference/Compendium.java
+++ src/main/java/werkbench/reference/Compendium.java
@@ -4,165 +4,16 @@ import cpw.mods.fml.common.ModMetadata;
 import java.util.Arrays;
 import net.minecraft.util.ResourceLocation;
 
-/*
- * A compendium of all constants for the mod, such as the modID and
- * ResourceLocations, as well as more general things like color codes
+/**
+ * A compendium of constants for the mod, such as the ID and ResourceLocations
+ *
+ * @author jakimfett
  */
 public class Compendium
 {
     public static enum AdjacentBlockType
     {
-        CHEST_DOUBLE(new int[]
-        {
-            0, 0
-        }, new int[]
-        {
-            54, 0
-        }, new int[]
-        {
-            0, 0
-        }, new int[]
-        {
-            0, 0
-        }),
-        CHEST_SINGLE(new int[]
-        {
-            54, 0
-        }, new int[]
-        {
-            0, 0
-        }, new int[]
-        {
-            57, 17
-        }, new int[]
-        {
-            0, 0
-        }),
-        EMPTY(new int[]
-        {
-            0, 0
-        }, new int[]
-        {
-            0, 0
-        }, new int[]
-        {
-            0, 0
-        }, new int[]
-        {
-            0, 0
-        }),
-        FURNACE_ACTIVE(new int[]
-        {
-            47, 13
-        }, new int[]
-        {
-            1, 13
-        }, new int[]
-        {
-            46, 10
-        }, new int[]
-        {
-            0, 10
-        }),
-        FURNACE_INACTIVE(new int[]
-        {
-            47, 13
-        }, new int[]
-        {
-            1, 13
-        }, new int[]
-        {
-            46, 10
-        }, new int[]
-        {
-            0, 10
-        }),
-        OFFSET(new int[]
-        {
-            8, 38
-        }, new int[]
-        {
-            306, 38
-        }, new int[]
-        {
-            0, 30
-        }, new int[]
-        {
-            298, 30
-        });
-
-        public static int[] getGUIBackgroundCoordinates(RelativeBenchSide side, AdjacentBlockType type)
-        {
-            if (side == RelativeBenchSide.LEFT)
-            {
-                return new int[]
-                {
-                    type.guiLeft[0] + AdjacentBlockType.OFFSET.guiLeft[0],
-                    type.guiLeft[1] + AdjacentBlockType.OFFSET.guiLeft[1]
-                };
-            } else
-            {
-                return new int[]
-                {
-                    type.guiRight[0] + AdjacentBlockType.OFFSET.guiRight[0],
-                    type.guiRight[1] + AdjacentBlockType.OFFSET.guiRight[1]
-                };
-            }
-        }
-
-        public static int[] getGUISlotCoordinates(RelativeBenchSide side, AdjacentBlockType type)
-        {
-            if (side == RelativeBenchSide.LEFT)
-            {
-                return new int[]
-                {
-                    type.slotLeft[0] + AdjacentBlockType.OFFSET.slotLeft[0],
-                    type.slotLeft[1] + AdjacentBlockType.OFFSET.slotLeft[1]
-                };
-            } else
-            {
-                return new int[]
-                {
-                    type.slotRight[0] + AdjacentBlockType.OFFSET.slotRight[0],
-                    type.slotRight[1] + AdjacentBlockType.OFFSET.slotRight[1]
-                };
-            }
-        }
-        private final int[] guiLeft, guiRight;
-        private final int[] slotLeft;
-        private final int[] slotRight;
-
-        AdjacentBlockType(int[] slotLeft, int[] slotRight, int[] guiLeft, int[] guiRight)
-        {
-            this.slotLeft = slotLeft;
-            this.slotRight = slotRight;
-            this.guiLeft = guiLeft;
-            this.guiRight = guiRight;
-        }
-
-    }
-
-    public static enum TabState
-    {
-        CLOSED, CLOSING,
-        OPEN, OPENING
-    }
-
-    public static enum RelativeBenchSide
-    {
-        LEFTBACK(0), BACK(1), BACKRIGHT(2), RIGHT(3), RIGHTFRONT(4), FRONT(5), FRONTLEFT(6), LEFT(7), TOP(8), BOTTOM(9);
-
-        private final int arraySlot;
-
-        RelativeBenchSide(int arraySlot)
-        {
-            this.arraySlot = arraySlot;
-        }
-
-        public static int getArraySlot(RelativeBenchSide side)
-        {
-            return side.arraySlot;
-        }
+        CHEST_DOUBLE, CHEST_SINGLE, EMPTY, FURNACE_ACTIVE, FURNACE_INACTIVE, OFFSET;
     }
 
     public static final class Naming
@@ -204,26 +55,18 @@ public class Compendium
         }
     }
 
-    public static final class Texture
+    public static final class TextureString
     {
 
-        public static final class IIcon
+        public static final class TAB
         {
-
-        }
-
-        public static final class Icon
-        {
-
+            public static final String chestBackground = "textures/gui/chestTabBackground.png";
+            public static final String furnaceBackground = "textures/gui/furnaceTabBackground.png";
         }
 
         public static final class GUI
         {
             public static final String background = "textures/gui/werkBenchGUIBackground.png";
-            public static final String chestTabBackground = "textures/gui/chestTabBackground.png";
-            public static final String doubleChest = "textures/gui/werkBenchGUIBackgroundChestDouble.png";
-            public static final String furnace = "textures/gui/werkBenchGUIBackgroundFurnace.png";
-            public static final String singleChest = "textures/gui/werkBenchGUIBackgroundChestSingle.png";
         }
 
         public static final class Model
@@ -233,54 +76,24 @@ public class Compendium
         }
     }
 
-    public static final class Resource
+    public static final class TextureResource
     {
-        public static final class Icon
-        {
-
-        }
 
         public static final class GUI
         {
-
-            public static final ResourceLocation background = new ResourceLocation(Compendium.Naming.id, Compendium.Texture.GUI.background);
-            public static final ResourceLocation singleChest = new ResourceLocation(Compendium.Naming.id, Compendium.Texture.GUI.singleChest);
-            public static final ResourceLocation doubleChest = new ResourceLocation(Compendium.Naming.id, Compendium.Texture.GUI.doubleChest);
-            public static final ResourceLocation furnace = new ResourceLocation(Compendium.Naming.id, Compendium.Texture.GUI.furnace);
+            public static final ResourceLocation background = new ResourceLocation(Compendium.Naming.id, Compendium.TextureString.GUI.background);
+            public static final ResourceLocation chest = new ResourceLocation(Compendium.Naming.id, Compendium.TextureString.TAB.chestBackground);
+            public static final ResourceLocation furnace = new ResourceLocation(Compendium.Naming.id, Compendium.TextureString.TAB.furnaceBackground);
         }
 
         public static final class Model
         {
-            public static final ResourceLocation bench = new ResourceLocation(Compendium.Naming.id, Compendium.Texture.Model.bench);
+            public static final ResourceLocation bench = new ResourceLocation(Compendium.Naming.id, Compendium.TextureString.Model.bench);
         }
     }
 
     public static final class Config
     {
-        public static final String configPrefix = "config/werkbench/";
-        public static final String assetPrefix = "/assets/werkbench/";
         public static final String categoryPerformance = "performance";
-    }
-
-    public static final class Color
-    {
-
-        public static final String black = "\u00A70";
-        public static final String darkBlue = "\u00A71";
-        public static final String darkCyan = "\u00A73";
-        public static final String darkGreen = "\u00A72";
-        public static final String darkGrey = "\u00A78";
-        public static final String darkRed = "\u00A74";
-        public static final String lightBlue = "\u00A79";
-        public static final String lightCyan = "\u00A7b";
-        public static final String lightGreen = "\u00A7a";
-        public static final String lightGrey = "\u00A77";
-        public static final String lightRed = "\u00A7c";
-        public static final String orange = "\u00A76";
-        public static final String pink = "\u00A7d";
-        public static final String purple = "\u00A75";
-        public static final String white = "\u00A7f";
-        public static final String yellow = "\u00A7e";
-
     }
 }
