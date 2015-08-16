bc843ecfc3db9bbe49152c65ccc84b86b8584b37
jakimfett <jakimfett@picraft.com>
2015-08-16 15:04:52 -0700

    Use new tab updating mechanic

--- src/main/java/werkbench/bench/BenchGUI.java
+++ src/main/java/werkbench/bench/BenchGUI.java
@@ -2,17 +2,8 @@ package werkbench.bench;
 
 import jakimbox.prefab.gui.BasicTabbedGUI;
 import jakimbox.prefab.gui.Tabs;
-import jakimbox.prefab.gui.Tabs.TabType;
-import jakimbox.prefab.gui.tabTypes.AnvilTab;
-import jakimbox.prefab.gui.tabTypes.ChestTab;
-import jakimbox.prefab.gui.tabTypes.FurnaceTab;
-import jakimbox.reference.RelativeDirection;
-import java.util.Map;
-import net.minecraft.block.Block;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
-import werkbench.reference.Compendium;
 import werkbench.reference.Compendium.Naming;
 
 public class BenchGUI extends BasicTabbedGUI
@@ -25,30 +16,6 @@ public class BenchGUI extends BasicTabbedGUI
     {
         super(Naming.id, inventoryPlayer, bench, world, 8);
 
-        tabs = new Tabs(8);
-        int indexCounter = 0;
-
-        for (Map.Entry<RelativeDirection, Block> entry : bench.getBlockCache().entrySet())
-        {
-            if (entry.getValue() == Blocks.chest)
-            {
-                // @TODO - add logic to check for double chests
-                tabs.addTab(new ChestTab(Compendium.Naming.id, RelativeDirection.getRelativeDirectionTabSide(entry.getKey()), TabType.CHEST_SINGLE), indexCounter);
-                indexCounter++;
-            } else if (entry.getValue() == Blocks.ender_chest)
-            {
-                tabs.addTab(new ChestTab(Compendium.Naming.id, RelativeDirection.getRelativeDirectionTabSide(entry.getKey()), TabType.CHEST_ENDER), indexCounter);
-                indexCounter++;
-            } else if (entry.getValue() == Blocks.furnace)
-            {
-                tabs.addTab(new FurnaceTab(Compendium.Naming.id, RelativeDirection.getRelativeDirectionTabSide(entry.getKey())), indexCounter);
-                indexCounter++;
-            } else if (entry.getValue() == Blocks.anvil)
-            {
-                tabs.addTab(new AnvilTab(Compendium.Naming.id, RelativeDirection.getRelativeDirectionTabSide(entry.getKey())), indexCounter);
-                indexCounter++;
-            }
-        }
         textureWidth = 222;
         xSize = textureWidth;
         ySize = textureHeight;
