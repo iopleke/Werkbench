8e95d4678caf9ae807544033e7f10e91752aee76
jakimfett <jakimfett@gmail.com>
2015-09-04 19:25:18 -0700

    Use JakimBox TESR class

--- src/main/java/werkbench/bench/BenchTileEntityRenderer.java
+++ src/main/java/werkbench/bench/BenchTileEntityRenderer.java
@@ -1,6 +1,7 @@
 package werkbench.bench;
 
-import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import jakimbox.prefab.render.BasicTileEntityRenderer;
+import jakimbox.prefab.tileEntity.BasicTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import org.lwjgl.opengl.GL11;
 import werkbench.reference.Compendium;
@@ -9,25 +10,41 @@ import werkbench.reference.Compendium;
  *
  * @author jakimfett
  */
-public class BenchTileEntityRenderer extends TileEntitySpecialRenderer
+public class BenchTileEntityRenderer extends BasicTileEntityRenderer
 {
 
-    private BenchModel benchModel = new BenchModel();
-
-    @Override
-    public void renderTileEntityAt(TileEntity tileEntity, double xCoord, double yCoord, double zCoord, float partialTick)
+    /**
+     * Rendering object with scale and rotation
+     */
+    public BenchTileEntityRenderer()
     {
-        GL11.glPushMatrix();
-        GL11.glTranslated(xCoord + 0.5, yCoord + 0.74, zCoord + 0.5);
-        GL11.glRotatef(180f, 0f, 0f, 1f);
-        GL11.glRotatef((tileEntity.getBlockMetadata() * 90.0F), 0.0F, 1.0F, 0.0F);
-        GL11.glEnable(GL11.GL_BLEND);
-        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        super(0.03125F);
+        this.model = new BenchModel();
+        this.texture = Compendium.TextureResource.Model.bench;
+        setOffset(0.5D, 0.74D, 0.5D);
 
-        bindTexture(Compendium.TextureResource.Model.bench);
-        this.benchModel.render(0.03125F);
-        GL11.glDisable(GL11.GL_BLEND);
-        GL11.glPopMatrix();
+        setRotation(0F, 0F, 180F);
     }
 
+    @Override
+    public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float partialTick)
+    {
+        if (tileEntity instanceof BasicTileEntity)
+        {
+
+            GL11.glPushMatrix();
+            GL11.glTranslated(x + xOffset, y + yOffset, z + zOffset);
+
+            doModelRotations();
+
+            GL11.glRotatef((tileEntity.getBlockMetadata() * 90.0F), 0.0F, 1.0F, 0.0F);
+            GL11.glEnable(GL11.GL_BLEND);
+            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+
+            bindTexture(texture);
+            model.render(0.03125F);
+            GL11.glDisable(GL11.GL_BLEND);
+            GL11.glPopMatrix();
+        }
+    }
 }
